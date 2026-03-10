d50c39d6f77cc616c699ec419a1d01a84d9286ae
annrancodes <147722113+annrancodes@users.noreply.github.com>
2026-03-10 12:36:36 +0530

    Obscure phone numbers in trigger_call.py

--- trigger_call.py
+++ trigger_call.py
@@ -6,8 +6,8 @@ auth_token = "XXXX"
 client = plivo.RestClient(auth_id, auth_token)
 
 response = client.calls.create(
-    from_="+14692463987",     # Plivo number
-    to_="+918031274121",      # Destination number
+    from_="+XXXXXX",     # Plivo number
+    to_="+XXXXXX",      # Destination number
     answer_url="https://appallingly-populationless-eun.ngrok-free.dev/ivr",
     answer_method="GET"
 )
